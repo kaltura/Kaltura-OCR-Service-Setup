59b90045f1f31f78a379bf3147a38cbe69dbbef2
Jess Portnoy <jess.portnoy@kaltura.com>
2015-09-04 15:40:35 +0100

    - search for all caption assets with label = "In-Video Text (OCR)" for entries that have the OCR set to Yes in the IBMOCR metadate profile and delete them

---
+++ ocr_cleanup.php
@@ -0,0 +1,67 @@
+<?php
+require_once('util_funcs.inc');
+
+
+if ($argc < 4){
+    echo "\nUsage: ".$argv[0] . ' <partner id> <admin secret> <service url>'."\n\n";
+    exit (1);
+}
+require_once('/opt/kaltura/web/content/clientlibs/php5/KalturaClient.php');
+define('METADATA_FIELD','ProcessOCR');
+define('METADATA_SYSTEM_NAME','IBMOCR');
+define('CAPTION_LABEL_TO_SEARCH','In-Video Text (OCR)');
+$userId = null;
+$expiry = null;
+$privileges = null;
+$partnerId=$argv[1];
+$secret = $argv[2];
+$type = KalturaSessionType::ADMIN;
+$config = new KalturaConfiguration($partnerId);
+$config->serviceUrl = $argv[3];
+$client = new KalturaClient($config);
+$ks = $client->session->start($secret, $userId, $type, $partnerId, $expiry, $privileges);
+$client->setKs($ks);
+
+$pager=null;
+$filter = new KalturaMetadataProfileFilter();
+$profile_name=METADATA_SYSTEM_NAME;
+$filter->systemNameEqual = $profile_name;
+$results = $client->metadataProfile->listAction($filter, $pager);
+if ($results->totalCount){
+    $metadata_prof_id=$results->objects[0]->id;
+    $metadataPlugin = KalturaMetadataClientPlugin::get($client);
+    $result = $metadataPlugin->metadataProfile->listFields($metadata_prof_id);
+    $xpath=$result->objects[0]->xPath;
+    echo "Found $profile_name, ID $metadata_prof_id. Will use $xpath to search.\n";
+}else{
+    echo "Could not find $profile_name. Exiting.\n";
+    exit (1);
+}
+
+$filter = new KalturaMediaEntryFilter(); 
+$filterAdvancedSearch = new KalturaMetadataSearchItem();
+$filterAdvancedSearch->metadataProfileId = $metadata_prof_id;
+ 
+$filterAdvancedSearchItems = new KalturaSearchCondition();
+$filterAdvancedSearchItems->field = $xpath; // Replace FieldName with the name obtained by calling metadataProfile service and showing defined fields
+$filterAdvancedSearchItems->value = 'Yes';
+ 
+$filterAdvancedSearch->items = array($filterAdvancedSearchItems);
+$filter->advancedSearch = $filterAdvancedSearch;
+
+$total_media_entries = $client->media->count($filter);
+$pager = new KalturaFilterPager();
+$page_index=1;
+$pager->pageSize = 500;
+$processed_entries=0;
+ 
+
+while ($processed_entries < $total_media_entries){
+    $pager->pageIndex=$page_index;
+    $result = $client->media->listAction($filter, $pager);
+	foreach ($result->objects as $entry) {
+	    $processed_entries++;
+	    delete_caption_assets($client,$entry->id,CAPTION_LABEL_TO_SEARCH);
+	}
+    $page_index++;
+}
